43a722dea37daac3b25b55397eb42cbc3521f1ab
allysalib <43250085+allysalib@users.noreply.github.com>
2020-11-29 21:57:02 -0500

    Update Bias_and_BiasMF_Models.py

--- Bias_and_BiasMF_Models.py
+++ Bias_and_BiasMF_Models.py
@@ -227,7 +227,8 @@ for file in json_file_list_subset:
     data = pd.read_json(file, lines=True, orient = 'columns')
     dfs.append(data)
 
-full_data = pd.concat(dfs)
+full_data1 = pd.concat(dfs)
+full_data = full_data1.reset_index(drop=True)
 
 ratings_data = full_data.rename(columns={'user_id':'user', 'business_id':'item', 'review_stars':'rating'})
 ratings_data['rating_binary'] = 0
