08e7a3379bef60ebd7eb61860d416a150bb9d987
allysalib <43250085+allysalib@users.noreply.github.com>
2020-12-09 07:23:36 -0500

    Update Bias_and_BiasMF_Models.py

--- Bias_and_BiasMF_Models.py
+++ Bias_and_BiasMF_Models.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import numpy as np
 pd.set_option('display.max_columns', None)
-import os, json
-import glob
 import numba
 import concurrent.futures
 from lenskit.datasets import MovieLens
@@ -19,36 +16,11 @@ from lenskit import batch, topn, util
 from lenskit import crossfold as xf
 from lenskit.algorithms import Recommender, als, item_knn as knn
 
-#small data
+data = pd.read_csv('/scratch/as12453/Subset/base_subset.csv')
 
-json_file_list_subset = ['/scratch/as12453/base_data_restauants_subset_unique.json/part-00000-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00001-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00002-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00003-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00004-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00005-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00006-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00007-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00008-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00009-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00010-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json',
-'/scratch/as12453/base_data_restauants_subset_unique.json/part-00011-e8e3aebd-9381-4252-bafa-acb9e82eecb0-c000.json']
-
-dfs = []
-
-for file in json_file_list_subset:
-    data = pd.read_json(file, lines=True, orient = 'columns')
-    dfs.append(data)
-    
-full_data = pd.concat(dfs)
-
-full_data = full_data.loc[~full_data.index.duplicated(keep='first')]
-
-full_data2 = full_data.reset_index()
-
-ratings_data = full_data2[['user_id', 'business_id', 'review_stars']].rename(columns={'user_id':'user', 'business_id':'item', 'review_stars':'rating'})
-ratings_data['rating_binary'] = 0
-ratings_data.loc[(ratings_data['rating'] > 3), 'rating_binary'] = 1
+ratings = data.rename(columns={'user_id':'user', 'business_id':'item', 'review_stars':'rating'})
+ratings['rating_binary'] = 0
+ratings.loc[(ratings['rating'] > 3), 'rating_binary'] = 1
 
 FRAC_SPLIT = 0.2
 N_SPLIT = 5
@@ -60,20 +32,20 @@ def fit_eval(aname, algo, train, test):
     users = test.user.unique()
     # now we run the recommender
     recs_10 = batch.recommend(model, users, 10)
-    recs_100 = batch.recommend(model, users, 100)
+    #recs_100 = batch.recommend(model, users, 100)
     # add the algorithm name for analyzability
     recs_10['Algorithm'] = aname
-    recs_100['Algorithm'] = aname
-    return model, recs_10, recs_100
+    #recs_100['Algorithm'] = aname
+    return model, recs_10
 
 def test_eval(model, test):
     users = test.user.unique()
     recs_10 = batch.recommend(model, users, 10)
-    recs_100 = batch.recommend(model, users, 100)
-    return recs_10, recs_100
+    #recs_100 = batch.recommend(model, users, 100)
+    return recs_10
 
 def main():
-    damping_values = [0, 1, 5, 10, 15]
+    damping_values = [0, 2, 5, 10]
 
     bias_test_splits = []
     bias_validation_prediction_scores_list = []
@@ -81,13 +53,15 @@ def main():
     bias_models = []
 
     for d in damping_values:
+        print(d)
         count = 0
-        for train_val, test in partition_users(ratings_data, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec=13), rng_spec=13):
+        for train_val, test in partition_users(ratings, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec=111), rng_spec=111):
             bias_test_splits.append(test)
-            for train, val in partition_users(train_val, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec=13), rng_spec=13):
+            for train, val in partition_users(train_val, 1, SampleFrac(FRAC_SPLIT, rng_spec=111), rng_spec=111):
                 count += 1
+                print(count)
                 B = Bias(items=True, users=False, damping=d)
-                model, recs_10, recs_100 = fit_eval("Bias, Damping={}".format(d), B, train, val)
+                model, recs_10 = fit_eval("Bias, Damping={}".format(d), B, train, val)
                 bias_models.append([(count-1), d, model])
                 predictions = model.predict(val[['user', 'item']])
                 rmse_score = rmse(predictions, val['rating'])
@@ -99,12 +73,12 @@ def main():
                 rla.add_metric(topn.precision)
                 rla.add_metric(topn.recall)
                 rla.add_metric(topn.ndcg)
-                results_10recs = rla.compute(recs_10, val_binary)
-                evals_10 = results_10recs[['recip_rank', 'precision', 'recall', 'ndcg']].rename(columns={"precision": "precision@10", "recall": "recall@10"})
-                results_100recs = rla.compute(recs_100, val_binary)
-                evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
-                evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
-                bias_validation_evals_list.append([(count-1), d, evals_full])
+                results_5recs = rla.compute(recs_10, val_binary)
+                evals_5 = results_5recs[['recip_rank', 'precision', 'recall', 'ndcg']].rename(columns={"precision": "precision@10", "recall": "recall@10"})
+                #results_100recs = rla.compute(recs_100, val_binary)
+                #evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
+                #evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
+                bias_validation_evals_list.append([(count-1), d, evals_10])
 
     bias_validation_prediction_scores = pd.DataFrame(bias_validation_prediction_scores_list, columns = ['split', 'damping_factor', 'rmse', 'mae'])              
 
@@ -121,7 +95,7 @@ def main():
     print("Bias models validation prediction scores by split:")
     print(bias_validation_prediction_scores)
 
-    bias_validation_evals_splits = bias_validation_evals_df.groupby(['damping_factor', 'split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    bias_validation_evals_splits = bias_validation_evals_df.groupby(['damping_factor', 'split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
     print("Bias models validation evaluations by split:")
     print(bias_validation_evals_splits)
 
@@ -129,7 +103,7 @@ def main():
     print("Aggregated bias models validation prediction scores by parameters:")
     print(bias_validation_prediction_scores_aggregated)
 
-    bias_validation_evals_aggregated = bias_validation_evals_df.groupby(['damping_factor']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    bias_validation_evals_aggregated = bias_validation_evals_df.groupby(['damping_factor']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
     print("Aggregated bias models validation evaluations by parameters:")
     print(bias_validation_evals_aggregated)
 
@@ -157,7 +131,7 @@ def main():
         mae_score = mae(predictions, test_data['rating'])
         bias_test_prediction_scores_list.append([count, rmse_score, mae_score])
         test_binary = test_data[['user', 'item', 'rating_binary']].rename(columns={"rating_binary": "rating"})
-        recs_10, recs_100 = test_eval(model, test_data)
+        recs_10 = test_eval(model, test_data)
         rla = topn.RecListAnalysis()
         rla.add_metric(topn.recip_rank)
         rla.add_metric(topn.precision)
@@ -165,10 +139,10 @@ def main():
         rla.add_metric(topn.ndcg)
         results_10recs = rla.compute(recs_10, test_binary)
         evals_10 = results_10recs[['recip_rank', 'precision', 'recall', 'ndcg']].rename(columns={"precision": "precision@10", "recall": "recall@10"})
-        results_100recs = rla.compute(recs_100, test_binary)
-        evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
-        evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
-        bias_test_evals_list.append([count, evals_full])
+        #results_100recs = rla.compute(recs_100, test_binary)
+        #evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
+        evals_full = pd.concat([evals_10], axis=1, sort=False)
+        bias_test_evals_list.append([count, evals_10])
 
     bias_test_prediction_scores = pd.DataFrame(bias_test_prediction_scores_list, columns = ['split', 'rmse', 'mae'])              
 
@@ -184,7 +158,7 @@ def main():
     print("Bias models test prediction scores by split:")
     print(bias_test_prediction_scores)
 
-    bias_test_evals_splits = bias_test_evals_df.groupby(['split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    bias_test_evals_splits = bias_test_evals_df.groupby(['split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
     print("Bias models test evaluations by split:")
     print(bias_test_evals_splits)
 
@@ -192,12 +166,12 @@ def main():
     print("Aggregated Bias models test prediction scores for best parameters:")
     print(bias_test_prediction_scores_aggregated)
 
-    bias_test_evals_aggregated = bias_test_evals_df.agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    bias_test_evals_aggregated = bias_test_evals_df.agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'],  'recall@10': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
     print("Aggregated Bias models test evaluations for best parameters:")
     print(bias_test_evals_aggregated)
 
-    regularization_values = [0, 0.1, 0.5, 0.75, 1]
-    dimensionality_values = [50, 250, 500]
+    regularization_values = [0, 0.1, 0.5]
+    dimensionality_values = [50, 250]
 
     biasMF_test_splits = []
     biasMF_validation_prediction_scores_list = []
@@ -207,12 +181,12 @@ def main():
     for r in regularization_values:
         for f in dimensionality_values:
             count = 0
-            for train_val, test in partition_users(ratings_data, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec=13), rng_spec=13):
+            for train_val, test in partition_users(ratings2, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec= 111), rng_spec=111):
                 biasMF_test_splits.append(test)
-                for train, val in partition_users(train_val, 1, SampleFrac(FRAC_SPLIT, rng_spec=13), rng_spec=13):
+                for train, val in partition_users(train_val, N_SPLIT, SampleFrac(FRAC_SPLIT, rng_spec=111), rng_spec=111):
                     count += 1
-                    BMF = BiasedMF(features=f, reg=r, rng_spec=13)
-                    model, recs_10, recs_100 = fit_eval("BiasMF, Reg={},Dim={}".format(r,f), BMF, train, val)
+                    BMF = BiasedMF(features=f, reg=r, rng_spec=111)
+                    model, recs_10 = fit_eval("BiasMF, Reg={},Dim={}".format(r,f), BMF, train, val)
                     biasMF_models.append([(count-1), r, f, model])
                     predictions = model.predict(val[['user', 'item']])
                     rmse_score = rmse(predictions, val['rating'])
@@ -226,10 +200,10 @@ def main():
                     rla.add_metric(topn.ndcg)
                     results_10recs = rla.compute(recs_10, val_binary)
                     evals_10 = results_10recs[['recip_rank', 'precision', 'recall', 'ndcg']].rename(columns={"precision": "precision@10", "recall": "recall@10"})
-                    results_100recs = rla.compute(recs_100, val_binary)
-                    evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
-                    evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
-                    biasMF_validation_evals_list.append([(count-1), r, f, evals_full])
+                    #results_100recs = rla.compute(recs_100, val_binary)
+                    #evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
+                    #evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
+                    biasMF_validation_evals_list.append([(count-1), r, f, evals_10])
 
     biasMF_validation_prediction_scores = pd.DataFrame(biasMF_validation_prediction_scores_list, columns = ['split', 'regularization', 'features', 'rmse', 'mae'])              
 
@@ -284,7 +258,7 @@ def main():
         mae_score = mae(predictions, test_data['rating'])
         biasMF_test_prediction_scores_list.append([count, rmse_score, mae_score])
         test_binary = test_data[['user', 'item', 'rating_binary']].rename(columns={"rating_binary": "rating"})
-        recs_10, recs_100 = test_eval(model, test_data)
+        recs_10 = test_eval(model, test_data)
         rla = topn.RecListAnalysis()
         rla.add_metric(topn.recip_rank)
         rla.add_metric(topn.precision)
@@ -292,10 +266,10 @@ def main():
         rla.add_metric(topn.ndcg)
         results_10recs = rla.compute(recs_10, test_binary)
         evals_10 = results_10recs[['recip_rank', 'precision', 'recall', 'ndcg']].rename(columns={"precision": "precision@10", "recall": "recall@10"})
-        results_100recs = rla.compute(recs_100, test_binary)
-        evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
-        evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
-        biasMF_test_evals_list.append([count, evals_full])
+        #results_100recs = rla.compute(recs_100, test_binary)
+        #evals_100 = results_100recs[['precision', 'recall']].rename(columns={"precision": "precision@100", "recall": "recall@100"})
+        #evals_full = pd.concat([evals_10, evals_100], axis=1, sort=False)
+        biasMF_test_evals_list.append([count, evals_10])
 
     biasMF_test_prediction_scores = pd.DataFrame(biasMF_test_prediction_scores_list, columns = ['split', 'rmse', 'mae'])              
 
@@ -311,7 +285,7 @@ def main():
     print("BiasMF models test prediction scores by split:")
     print(biasMF_test_prediction_scores)
 
-    biasMF_test_evals_splits = biasMF_test_evals_df.groupby(['split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    biasMF_test_evals_splits = biasMF_test_evals_df.groupby(['split']).agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
     print("BiasMF models test evaluations by split:")
     print(biasMF_test_evals_splits)
 
@@ -319,9 +293,9 @@ def main():
     print("Aggregated BiasMF models test prediction scores for best parameters:")
     print(biasMF_test_prediction_scores_aggregated)
 
-    biasMF_test_evals_aggregated = biasMF_test_evals_df.agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'precision@100': ['mean', 'std'], 'recall@10': ['mean', 'std'], 'recall@100': ['mean', 'std'], 'ndcg': ['mean', 'std'], })
+    biasMF_test_evals_aggregated = biasMF_test_evals_df.agg({'recip_rank': ['mean', 'std'], 'precision@10': ['mean', 'std'], 'recall@10': ['mean', 'std'],  'ndcg': ['mean', 'std'], })
     print("Aggregated BiasMF models test evaluations for best parameters:")
     print(biasMF_test_evals_aggregated)
-    
+
 if __name__ == '__main__':
     main()
